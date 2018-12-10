655b21c7b560cabedf54ed0bfe05524679c175ae
qsheng.wang <qsheng.wang@topdeep.com>
2018-12-10 13:18:59 +0800

    2018-12-10

--- src/main/java/com/miroslav/house/HouseApplication.java
+++ src/main/java/com/miroslav/house/HouseApplication.java
@@ -9,6 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HouseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HouseApplication.class, args);
+		SpringApplication.run(  HouseApplication.class, args);
 	}
 }
